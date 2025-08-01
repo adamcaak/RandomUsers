bbb621e2da2cfa8903a884f3b68f642bd98a3ae7
adamcaak <ladamczak1997@gmail.com>
2025-08-01 22:11:28 +0200

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Łukasz Adamczak on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomUsersTests/RandomUsersTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersTests.swift
+//  RandomUsersTests
+//
+//  Created by Łukasz Adamczak on 01/08/2025.
+//
+
+import Testing
+@testable import RandomUsers
+
+struct RandomUsersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
