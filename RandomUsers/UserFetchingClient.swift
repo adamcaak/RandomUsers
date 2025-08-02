4b998e4b568c98ce546b9c647172ab734873cdfd
adamcaak <ladamczak1997@gmail.com>
2025-08-02 13:22:45 +0200

    Update func getUsers

--- RandomUsers/UserFetchingClient.swift
+++ RandomUsers/UserFetchingClient.swift
@@ -10,7 +10,8 @@ import Foundation
 struct UserFetchingClient {
     static private let url = URL(string: "https://randomuser.me/api/?results=10&format=pretty")!
     
-    static func getUsers() async {
+    static func getUsers() async throws -> String {
         async let (data, _) = await URLSession.shared.data(from: url)
+        return try await String(data: data, encoding: .utf8)!
     }
 }
